b516530216dd7d2fa63393ac8716ca7ef07abd7c
jciffer <jciffer@HS-D110-23.MOHS.local>
2017-12-19 09:34:09 -0500

    Hi

--- DatFile/src/src/datFile/backend/DatFile.java
+++ DatFile/src/src/datFile/backend/DatFile.java
@@ -1,6 +1,12 @@
 package src.datFile.backend;
 
-
+/**
+ * REFERENCES:
+ * File API, printstream, filewriter, blue pelican lessons 23-25
+ * 
+ * @author Joshua Ciffer, Brian Williams
+ * @version 12/19/2017
+ */
 public class DatFile {
 
 }
